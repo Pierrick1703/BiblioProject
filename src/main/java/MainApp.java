d1de8d06c083716c794de59430eace34becad6ce
Pierrick COTTIN <cottin.pierrick@gmail.com>
2022-03-29 15:15:35 +0200

    page connexion fini (dur)

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
+
+public class MainApp extends Application {
+
+    @Override
+
+    public void start(Stage stage) throws IOException {
+        //Object test = getClass().getResource("/main/resources/Authentification.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/resources/Authentification.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Bibliothèque - M1 M2I ESIEE IT");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    ///////////////// Sauvegarde /////////////////////////
+
+}
---
+++ src/main/java/controllers/Authentification.java
@@ -0,0 +1,55 @@
+package main.java.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Authentification {
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button validateButton;
+    @FXML private Button cancelButton;
+
+    //ce login en tant que différent user
+    @FXML
+    void validaton(ActionEvent event) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        //obligation d'utiliser contains than username == "pcot"
+        if(username.contains("pcot") || username.contains("jkla")  || username.contains("cgob")){//accès simple
+            JFrame jFrame = new JFrame();
+            if(password.contains("1234")){
+                /*start();*/
+            } else {
+                JOptionPane.showMessageDialog(jFrame, "Mot de passe incorrect");
+            }
+        } else if(username.contains("pcotin")  || username.contains("jklatt") || username.contains("cgobert")) {//accès admin
+
+        } else {
+            JFrame jFrame = new JFrame();
+            JOptionPane.showMessageDialog(jFrame, "Nom d'utilisateur incorrect");
+        }
+    }
+    //arrêter le programme
+    @FXML
+    void fermeture(ActionEvent event) {
+        System.exit(0);
+    }
+
+    /*
+    public void start() throws IOException {
+        //Object test = getClass().getResource("/main/resources/Authentification.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/main/resources/Authentification.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage ;
+        stage.setTitle("Bibliothèque - M1 M2I ESIEE IT");
+        stage.setScene(scene);
+        stage.show();
+    }*/
+}
